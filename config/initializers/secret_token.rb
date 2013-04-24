9c4dce20180c03ee8dfd3d3baf24f8ce15346f71
CybLeBorg <remy.leynaud@gmail.com>
2013-04-24 21:44:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BienveillantApp::Application.config.secret_token = 'a58efb967bf2de1f8c0baf1f09048ca05055967c7e28a517794534b7f1a6ec60ab862f17c27cdc4ef11f67fac013f65a40686505fbfec09010264f8eb43dded6'
